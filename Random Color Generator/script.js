afc7e915b3a1d745187f4582a4d704dd4ed28256
Tanny02 <tanishmarick98@gmail.com>
2024-01-29 19:45:30 +0530

    Complete

--- Random Color Generator/script.js
+++ Random Color Generator/script.js
@@ -1,23 +1,26 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 30; i++) {
+for (let i = 0; i < 60; i++) {
     const color = document.createElement('div');
     color.classList.add('color');
     container.appendChild(color);
 }
-
+    
 const colorEls = document.querySelectorAll('.color');
 
 generateColor();
 
 function generateColor() {
+    
     colorEls.forEach(colorEl => {
         colorEl.style.backgroundColor = randomValue();
         colorEl.innerText = randomValue();
     })
+    
 }
 
 function randomValue() {
+
     const chars = "0123456789abcdef";
     const colorCodeLength = 6;
     let color = "#";
